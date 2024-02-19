c013661ec2eb6a3badb909d8dc83be2f832ece7f
scarrscarr <postiga.esteban@gmail.com>
2024-02-19 21:53:34 +0100

    css

--- resources/views/plat.blade.php
+++ resources/views/plat.blade.php
@@ -7,7 +7,7 @@
     <img src="{{ asset('image/'.$plat->fichier) }}" alt="{{ $plat->nom }}" />
     <h2>{{ $plat->nom }}</h2>
     <span class="prix">{{ $plat->prix }} eur</span>
-    <ul>
+    <ul class="etik">
         @foreach ($plat->etiquettes() as $etiquette)
              <li>{{ $etiquette->nom }}</li>
          @endforeach
